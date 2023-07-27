d97a63a301ea694b9e6576527dddb3307cbbfc3e
Guzman Rivero <guzmanrivero@Air-de-Guzman.jtc.local>
2023-07-27 14:07:45 -0300

    add trycatch in setPurchaseOrder

--- models/jtc_setting_serial_num_purchase_MSR.ts
+++ models/jtc_setting_serial_num_purchase_MSR.ts
@@ -28,80 +28,84 @@ export const afterSubmit = (ctx: EntryPoints.UserEvent.afterSubmitContext) => {
 
 const setPurchaseOrder = (idPurchaseOrder, numLoteArr: Array<[]>, intItem, dtValidade) => {
 
-    const recordPurchaseOrder = record.load({
-        type: record.Type.PURCHASE_ORDER,
-        id: idPurchaseOrder, 
-        isDynamic: true
-    });
-
-    const lineCount = recordPurchaseOrder.getLineCount({sublistId: cts.PURCHASE_ORDER.SUBLIST_ITEM.ID});
-
-    for (var i=0; i < lineCount; i++) {
-        recordPurchaseOrder.selectLine({
-            sublistId: cts.PURCHASE_ORDER.SUBLIST_ITEM.ID,
-            line: i
+    try {
+        const recordPurchaseOrder = record.load({
+            type: record.Type.PURCHASE_ORDER,
+            id: idPurchaseOrder, 
+            isDynamic: true
         });
-
-        const item = recordPurchaseOrder.getCurrentSublistValue({
-            sublistId: cts.PURCHASE_ORDER.SUBLIST_ITEM.ID,
-            fieldId: cts.PURCHASE_ORDER.SUBLIST_ITEM.ITEM
-        })
-
-        const inventoryDetaiRec = recordPurchaseOrder.getCurrentSublistSubrecord({
-            sublistId: cts.PURCHASE_ORDER.SUBLIST_ITEM.ID,
-            fieldId: cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.ID_SUBRECORD
-        });
-
-
-        const lineCountInventoryDetail = inventoryDetaiRec.getLineCount({sublistId: cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.ID});
-        
-        if (lineCountInventoryDetail == 0 &&  item == intItem) {
+    
+        const lineCount = recordPurchaseOrder.getLineCount({sublistId: cts.PURCHASE_ORDER.SUBLIST_ITEM.ID});
+    
+        for (var i=0; i < lineCount; i++) {
+            recordPurchaseOrder.selectLine({
+                sublistId: cts.PURCHASE_ORDER.SUBLIST_ITEM.ID,
+                line: i
+            });
+    
+            const item = recordPurchaseOrder.getCurrentSublistValue({
+                sublistId: cts.PURCHASE_ORDER.SUBLIST_ITEM.ID,
+                fieldId: cts.PURCHASE_ORDER.SUBLIST_ITEM.ITEM
+            })
+    
+            const inventoryDetaiRec = recordPurchaseOrder.getCurrentSublistSubrecord({
+                sublistId: cts.PURCHASE_ORDER.SUBLIST_ITEM.ID,
+                fieldId: cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.ID_SUBRECORD
+            });
+    
+    
+            const lineCountInventoryDetail = inventoryDetaiRec.getLineCount({sublistId: cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.ID});
             
-            for (var j=0; j < numLoteArr.length; j+=2) {
+            if (lineCountInventoryDetail == 0 &&  item == intItem) {
                 
-                inventoryDetaiRec.selectNewLine({
-                    sublistId: cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.ID,
-                });
-
-                
-                
-                
-                
-                inventoryDetaiRec.setCurrentSublistValue({
-                    fieldId: cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.DATA_VALIDADE,
-                    sublistId: cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.ID,
-                    value: new Date(dtValidade)
-                });
-                log.debug("dtValidade", dtValidade);
-
-                inventoryDetaiRec.setCurrentSublistValue({
-                    fieldId: cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.QUANTITY,
-                    sublistId: cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.ID,
-                    value: numLoteArr[j+1]
-                });
-                log.debug("qtde", numLoteArr[j+1])
-
-                inventoryDetaiRec.setCurrentSublistValue({
-                    fieldId: cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.SET_SERIAL_NUMBER,
-                    sublistId:  cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.ID,
-                    value: numLoteArr[j]
-                });
-                log.debug("numSerial", numLoteArr[j]);
-                inventoryDetaiRec.commitLine({sublistId: cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.ID});
-
+                for (var j=0; j < numLoteArr.length; j+=2) {
+                    
+                    inventoryDetaiRec.selectNewLine({
+                        sublistId: cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.ID,
+                    });
+    
+                    
+                    
+                    
+                    
+                    inventoryDetaiRec.setCurrentSublistValue({
+                        fieldId: cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.DATA_VALIDADE,
+                        sublistId: cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.ID,
+                        value: new Date(dtValidade)
+                    });
+                    log.debug("dtValidade", dtValidade);
+    
+                    inventoryDetaiRec.setCurrentSublistValue({
+                        fieldId: cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.QUANTITY,
+                        sublistId: cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.ID,
+                        value: numLoteArr[j+1]
+                    });
+                    log.debug("qtde", numLoteArr[j+1])
+    
+                    inventoryDetaiRec.setCurrentSublistValue({
+                        fieldId: cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.SET_SERIAL_NUMBER,
+                        sublistId:  cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.ID,
+                        value: numLoteArr[j]
+                    });
+                    log.debug("numSerial", numLoteArr[j]);
+                    inventoryDetaiRec.commitLine({sublistId: cts.PURCHASE_ORDER.SUBLIST_INVENTORY_DETAIL.ID});
+    
+                }
+    
             }
-
+    
+    
+            recordPurchaseOrder.commitLine({sublistId: cts.PURCHASE_ORDER.SUBLIST_ITEM.ID});
+            
+    
         }
-
-
-        recordPurchaseOrder.commitLine({sublistId: cts.PURCHASE_ORDER.SUBLIST_ITEM.ID});
-        
-
+    
+    
+        const returnIdPurcharOrder = recordPurchaseOrder.save({ignoreMandatoryFields: true});
+        log.debug("returnPurchaseOrder", returnIdPurcharOrder);
+    } catch (e) {
+        log.error("jtc_setting_serial_num_purchase_MSR.setPurchaseOrder", e);
     }
-
-
-    const returnIdPurcharOrder = recordPurchaseOrder.save({ignoreMandatoryFields: true});
-    log.debug("returnPurchaseOrder", returnIdPurcharOrder);
 
 }
 
